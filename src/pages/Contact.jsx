0f5f61ce39407ac6629425c67e30a6d002af3a03
Gaurav Thakur <gauravkumarmrt3@gmail.com>
2026-03-29 14:16:21 +0530

    Update Contact.jsx
    Signed-off-by: Gaurav Thakur <gauravkumarmrt3@gmail.com>

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import ServicesSection from "../components/ServicesSection";
 function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <h1>Contact Page gaurav thakur</h1>
       <ServicesSection />
     </div>
   );
