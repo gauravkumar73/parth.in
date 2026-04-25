dc49bc774416d130960379ca81eb2152e3585499
Gaurav Thakur <gauravkumarmrt3@gmail.com>
2026-04-25 09:11:29 +0530

    Reorder and uncomment Testimonials component
    Signed-off-by: Gaurav Thakur <gauravkumarmrt3@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import ServicesSection from "../components/ServicesSection";
 import Industries from "../components/Industries";
-import Testimonials from "../components/Testimonials";
+
 import OfferSection from "../components/OfferSection";
 import BannerSlider from "../components/BannerSlider";
 import Cards from "../components/Cards";
+import Testimonials from "../components/Testimonials";
 function Home() {
   return (
     <div>
         <BannerSlider />
       <ServicesSection />
       <Industries />
-      <Testimonials />
+     
       <OfferSection />
       <Cards />
-   
+    <Testimonials />
     </div>
   );
 }
